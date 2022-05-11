bc203b73c57c16f514f21fec05658ec806732e86
lmagoncalo <lmagoncalo@hotmail.com>
2022-05-11 16:52:56 +0100

    - Debug

--- src/App.js
+++ src/App.js
@@ -8,16 +8,55 @@ import {Component} from "react";
 import Footer from "./Components/Footer";
 import NavBar from "./Components/NavBar";
 import {Container} from "react-bootstrap";
-import { socketID, socket } from './Components/socket';
+import io from 'socket.io-client';
+// import { socketID, socket } from './Components/socket';
+
+var connectionOptions =  {
+    "force new connection" : true,
+    "reconnectionAttempts": "Infinity",
+    "timeout" : 10000,
+    "transports" : ["websocket"]
+};
+
+let socket = io.connect('https://localhost:5000',connectionOptions);
 
 
 class App extends Component {
-
     constructor(props) {
         super(props);
 
         let c = Math.round(Math.random()*(AVAILABLE_COLOURS.length-1));
         this.typoc = AVAILABLE_COLOURS[c];
+    }
+
+    componentWillUnmount() {
+        this.socket.close();
+        console.log("component unmounted");
+    }
+
+    componentDidMount() {
+        var sensorEndpoint = "http://localhost:5000";
+        this.socket = io.connect(sensorEndpoint, {
+            reconnection: true,
+            // transports: ['websocket']
+        });
+        console.log("component mounted");
+
+        this.socket.on("new-pixel", data => {
+            // this.drawOnCanvas(this.state.p5, data.x, data.y, data.color);
+        });
+
+        this.socket.on('draw-pixels', data => {
+            console.log("AQUI, draw pixels");
+            /*
+            for (let i = 0; i < data.length; i++) {
+                let pixel = data[i];
+                // console.log(pixel);
+                this.drawOnCanvas(this.state.p5, pixel.x, pixel.y, pixel.color);
+            }
+            */
+        });
+
     }
 
     render () {
